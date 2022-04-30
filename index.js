0ced79a4611dfff98c9fc46b744da75e5dd9b7e7
Brandon Matrese <bmatrese@gmail.com>
2022-04-30 15:57:40 -0400

    commit tree 945dcfc67793db05f5b7c60560bdc14f3d6e6aec

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return someValue = 8;
+}
+
+function distanceFromHqInFeet(someValue) {
+    return someValue = 2112;
+}
+
+function distanceTravelledInFeet() {
+
+}
